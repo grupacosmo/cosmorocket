1513c00530a0e657706789dedd09cf20391904d2
regeliv <124085151+regeliv@users.noreply.github.com>
2024-06-27 11:52:33 +0000

    Gps parsing (#26)
    * Add gps data reading and logging
    
    * Modify .gitignore
    
    * Add pio makefile facilitating IDE-less development
    
    * Comment the purpose of log, delete macros

--- projects/T-beam/include/gps.h
+++ projects/T-beam/include/gps.h
@@ -1,11 +1,21 @@
 #include <HardwareSerial.h>
-#define GPS_SERIAL_NUM 1
-#define GPS_RX_PIN 34
-#define GPS_TX_PIN 12
 
 namespace gps {
+struct Time {
+  uint8_t hours{};
+  uint8_t minutes{};
+  uint8_t seconds{};
+};
+
+struct Data {
+  Time time{};
+  double lat{};
+  double lng{};
+};
 
 void init();
-void gps_loop(void* pvParameters);
+void gps_task(void *pvParameters);
+Data get_gps_data();
+bool data_is_available();
 
-}  // namespace gps
+} // namespace gps
--- projects/T-beam/src/main.cpp
+++ projects/T-beam/src/main.cpp
@@ -5,23 +5,43 @@
 #include "gps.h"
 #include "led.h"
 
+/// Log data from sensors
+/// For now it only logs gps data to console, eventually
+/// it should write the data from all components to a disk
+void log(void *pvParameters) {
+  for (;;) {
+    if (!gps::data_is_available()) {
+      Serial.println("GPS data is unavailable.");
+    } else {
+      gps::Data gps_data = gps::get_gps_data();
+      Serial.printf("Lat: %.6f Long: %.6f Time: %02d:%02d:%02d\n", gps_data.lat,
+                    gps_data.lng, gps_data.time.hours, gps_data.time.minutes,
+                    gps_data.time.seconds);
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
 void parser(void* pvParameters) {
   for (;;) {
     Serial.println(bmp::global_bmp.altitude);
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
+
 void setup() {
   Serial.begin(115200);
   Serial.println("--- Starting the rocket... ---");
   gps::init();
   xTaskCreate(led::blink_task, "blink", 10000, NULL, 1, NULL);
+
+  xTaskCreate(gps::gps_task, "gps", 10000, NULL, 1, NULL);
+  xTaskCreate(log, "log", 10000, NULL, 1, NULL);
   // xTaskCreate(gps::gps_loop, "gps", 10000, NULL, 1, NULL);
 
   // bmp::init();
   // xTaskCreate(bmp::get_bme, "bmp", 10000, NULL, 1, NULL);
   xTaskCreate(parser, "parser", 10000, NULL, 1, NULL);
-  xTaskCreate(gps::gps_loop, "gps", 10000, NULL, 1, NULL);
 }
 
 void loop() {}
