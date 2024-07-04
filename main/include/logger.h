f7862f414907e318cdce9c4a8dd70721c5cee18c
Serfer <bartosz.krzykk@gmail.com>
2024-07-04 16:14:13 +0200

    implement logger function

---
+++ main/include/logger.h
@@ -0,0 +1,17 @@
+#include "bmp.h"
+#include "gps.h"
+#include "mpu.h"
+#include <cstdint>
+
+namespace log {
+struct Packet {
+    bmp::Data bmp_data{};
+    mpu::Data mpu_data{};
+    gps::Data gps_data{};
+    std::uint32_t sys_time{};
+    std::uint16_t n{};
+    std::uint8_t status{};
+};
+
+String serialize(Packet &packet);
+} // namespace log
