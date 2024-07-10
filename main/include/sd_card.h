035f2a307c3800c7f0b2307759ccc64a2ad20aaa
Bartosz <bartosz.slowik@student.pk.edu.pl>
2024-07-10 21:46:13 +0200

    undo testing

--- main/include/sd_card.h
+++ main/include/sd_card.h
@@ -7,8 +7,8 @@ namespace sd {
 static constexpr std::uint8_t DEFAULT_DATA_PIN = 32U;
 
 void init();
-void write(const char *data, char *filename);
-void write(const String &data, String &filename);
+void write(const char *data);
+void write(const String &data);
 void read(const char *filename);
 void read(const String &filename);
 }; // namespace sd
