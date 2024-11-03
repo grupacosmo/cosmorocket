fa6849b02918fd30f9ced08147e061691268a044
madix <i@madix.dev>
2024-11-03 23:28:29 +0100

    Applied requested changes

--- main/include/mpu.h
+++ main/include/mpu.h
@@ -3,22 +3,22 @@
 namespace mpu {
 
 struct Position {
-  float x{};
-  float y{};
-  float z{};
+    float x{};
+    float y{};
+    float z{};
 };
 
 struct Data {
-  Position max{};
-  Position avg{};
-  Position rot{};
+    Position max{};
+    Position avg{};
+    Position rot{};
 };
 
 void init();
 bool calibrate();
 void mpu_task(void *pvParameters);
 void mpu_print(void *pvParameters);
-void pretty_print();
-Data get_mpu();
+void print_data();
+Data get_data();
 
 } // namespace mpu
--- main/src/mpu.cpp
+++ main/src/mpu.cpp
@@ -5,18 +5,13 @@ namespace mpu {
 
 MPU6050 mpudev;
 
-static SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
-static unsigned int count = 0;
-static Data internal{};
-static float gyro_res = 0, accel_res = 0;
-static bool init_success = false;
+unsigned int count = 0;
+Data internal{};
+float gyro_res = 0, accel_res = 0;
+bool init_success = false;
 
-/**
- * @brief Mpu initialization. Return gracefully on initialisation error.
- */
 void init() {
-    // Parameters change the resolution of the sensor.
-    mpudev.initialize(/*MPU6050_ACCEL_FS_2, MPU6050_GYRO_FS_250*/);
+    mpudev.initialize();
 
     // Calibration on start?
     // mpudev.CalibrateAccel(10);
@@ -32,14 +27,7 @@ void init() {
     // mpudev.setZGyroOffset(0);
 
     if (!mpudev.testConnection()) {
-        // die
         Serial.println("Connection to mpu failed");
-        return;
-    }
-
-    if (mutex == nullptr) {
-        // die
-        Serial.println("Couldn't allocate resources for mutex!");
         return;
     }
 
@@ -48,44 +36,31 @@ void init() {
     gyro_res = mpudev.get_gyro_resolution() / 2;
 }
 
-/**
- * @brief Consume and return collected data.
- *        ! CAN BLOCK INDEFINITELY (if INCLUDE_vTaskSuspend == 1) !
- * @return Max g force, Average g force, Average rotation.
- */
-Data get_mpu() {
-    xSemaphoreTake(mutex, portMAX_DELAY);
-
-    Data dane{
-        .max_x = internal.max_x,
-        .max_y = internal.max_y,
-        .max_z = internal.max_z,
-        .avg_x = internal.avg_x / count, // Instead of counting could implement
-        .avg_y = internal.avg_y / count, // moving average, but dunno if it's
-        .avg_z = internal.avg_z / count, // superior and/or viable alternative.
-        .rot_x = internal.rot_x / count,
-        .rot_y = internal.rot_y / count,
-        .rot_z = internal.rot_z / count};
+Data get_data() {
+    Data dane{};
+    dane.max.x = internal.max.x;
+    dane.max.y = internal.max.y;
+    dane.max.z = internal.max.z;
+    dane.avg.x = internal.avg.x / count,
+    dane.avg.y = internal.avg.y / count,
+    dane.avg.z = internal.avg.z / count,
+    dane.rot.x = internal.rot.x / count,
+    dane.rot.y = internal.rot.y / count,
+    dane.rot.z = internal.rot.z / count,
 
     count = 0;
     internal = {};
 
-    xSemaphoreGive(mutex);
-
     return dane;
 }
 
-/**
- * @brief Main MPU task that collects data from the sensor.
- *        Die gracefully if sensor not initialised.
- *        ! CAN BLOCK INDEFINITELY (if INCLUDE_vTaskSuspend == 1) !
- */
 void mpu_task([[maybe_unused]] void *pvParameters) {
     int16_t ax, ay, az, gx, gy, gz;
     float fax, fay, faz, fgx, fgy, fgz;
 
-    if (!init_success)
-        vTaskDelete(NULL);
+    if (!init_success) {
+        vTaskDelete(nullptr);
+    }
 
     for (;;) {
         mpudev.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
@@ -97,35 +72,31 @@ void mpu_task([[maybe_unused]] void *pvParameters) {
         fgy = abs(gy * gyro_res);
         fgz = abs(gz * gyro_res);
 
-        xSemaphoreTake(mutex, portMAX_DELAY);
+        if (internal.max.x < fax) {
+            internal.max.x = fax;
+        }
+        if (internal.max.y < fay) {
+            internal.max.y = fay;
+        }
+        if (internal.max.z < faz) {
+            internal.max.z = faz;
+        }
 
-        if (internal.max_x < fax)
-            internal.max_x = fax;
-        if (internal.max_y < fay)
-            internal.max_y = fay;
-        if (internal.max_z < faz)
-            internal.max_z = faz;
-
-        internal.avg_x += fax;
-        internal.avg_y += fay;
-        internal.avg_z += faz;
-        internal.rot_x += fgx;
-        internal.rot_y += fgy;
-        internal.rot_z += fgz;
+        internal.avg.x += fax;
+        internal.avg.y += fay;
+        internal.avg.z += faz;
+        internal.rot.x += fgx;
+        internal.rot.y += fgy;
+        internal.rot.z += fgz;
 
         count++;
-
-        xSemaphoreGive(mutex);
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-/**
- * @brief Consume and display collected data.
- */
-void pretty_print() {
-    Data data = get_mpu();
+void print_data() {
+    Data data = get_data();
     Serial.printf("[Max g force]\n"
                   "[x, y, z]\n"
                   "%f, %f, %f\n"
@@ -135,23 +106,9 @@ void pretty_print() {
                   "[Average rotation]\n"
                   "[x, y, z]\n"
                   "%f, %f, %f\n",
-                  data.max_x, data.max_y, data.max_z, data.avg_x, data.avg_y,
-                  data.avg_z, data.rot_x, data.rot_y, data.rot_z);
-    Serial.flush();
-}
-
-/**
- * @brief Task that consumes and displays collected data repeatedly.
- *        Die gracefully if sensor not initialised.
- */
-void mpu_print([[maybe_unused]] void *pvParameters) {
-    if (!init_success)
-        vTaskDelete(NULL);
-
-    for (;;) {
-        pretty_print();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+                  data.max.x, data.max.y, data.max.z,
+                  data.avg.x, data.avg.y, data.avg.z,
+                  data.rot.x, data.rot.y, data.rot.z);
 }
 
 } // namespace mpu
