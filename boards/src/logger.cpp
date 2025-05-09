17ef1f548284f170739b5db222a3f02bd5545faf
Tomasz Klimek <tomkl584@gmail.com>
2025-05-09 20:28:54 +0200

    Fix logger

--- boards/src/logger.cpp
+++ boards/src/logger.cpp
@@ -4,12 +4,15 @@ namespace logger {
 
 // Private
 namespace {
+constexpr size_t SERIALIZE_PACKET_SIZE = 1024;
+char buf[SERIALIZE_PACKET_SIZE];
+
 int serialize_data(char *buf, size_t len, const Packet &packet) {
   return snprintf(
       // clang-format off
       buf, len,
       "%u,%u:%u:%u,%u,%0.4f,%0.4f,%0.4f,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f",
-      packet.n, packet.gps_data.time.hours, packet.gps_data.time.minutes,
+      packet.n, packet.gps_data.time.hours, packet.gps_data.time.minutes, 
       packet.gps_data.time.seconds, packet.status,
       packet.bmp_data.temperature, packet.bmp_data.pressure, packet.bmp_data.altitude,
       packet.mpu_data.acc_max.x, packet.mpu_data.acc_max.y, packet.mpu_data.acc_max.z,
@@ -22,15 +25,13 @@ int serialize_data(char *buf, size_t len, const Packet &packet) {
 // clang-format on
 }  // namespace
 
-String serialize(Packet &packet) {
-  char buf[256];
-  if (!serialize_data(&buf[0], 256, packet)) {
+const char *serialize(Packet &packet) {
+  if (!serialize_data(&buf[0], SERIALIZE_PACKET_SIZE, packet)) {
     Serial.println("Failed to serialize data, check buffer size");
-    return String(
-        "0,0:0:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
+    serialize_data(&buf[0], SERIALIZE_PACKET_SIZE, Packet{});
   }
 
-  return String(buf);
+  return buf;
 }
 
 }  // namespace logger
