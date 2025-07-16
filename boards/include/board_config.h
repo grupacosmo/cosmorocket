360a382a7c1f907d7d5ba835fe5d39981ca45020
Bartosz Słowik <bartosz.slowik@student.pk.edu.pl>
2025-07-16 16:51:20 +0200

    fixed lolin pipeline

--- boards/include/board_config.h
+++ boards/include/board_config.h
@@ -29,6 +29,14 @@ constexpr int8_t SD_SS = 5;
 constexpr uint8_t SDA_PIN = 13;
 constexpr uint8_t SCL_PIN = 15;
 
+//pin 99 means unimplemented or not used
+constexpr uint8_t P1 = 99;
+constexpr uint8_t P2 = 99;
+constexpr uint8_t P3 = 99;
+constexpr uint8_t PT1 = 99;
+constexpr uint8_t PT2 = 99;
+constexpr uint8_t PT3 = 99;
+
 #endif
 
 constexpr unsigned long BAUD_RATE = 115200;
