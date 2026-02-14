f3315595b0e41bbcacd3fd1cf9a1af162fa9b76a
Tomasz Klimek <tomkl584@gmail.com>
2026-02-14 14:49:39 +0100

    Add script for downloading data from flash memory

---
+++ boards/download-data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "The script must be run in ESP-IDF Terminal - in VS Code it can be launched in"
+echo "View -> Command Palette -> ESP-IDF: Open ESP-IDF Terminal (or Ctrl + E T)"
+
+partition_size=$((14 * 1024 * 1024)) # 14MB
+
+parttool.py -b 1000000 read_partition --partition-name=littlefs --output "data-partition.bin"
+
+pip install littlefs-tools
+rm -r data-partition 2>/dev/null
+block_size=4096
+block_count=$(($partition_size / $block_size))
+littlefs_extract -b $block_size -c $block_count -d data-partition --image data-partition.bin
